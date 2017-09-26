f1d711ff73410def74640d218a6d96902dba5df9
EternalZero <jray12332@gmail.com>
2017-09-26 10:17:46 -0700

    Uneeded comments

--- Menu.java
+++ Menu.java
@@ -9,8 +9,6 @@ import java.awt.Rectangle;
 
 public class Menu{
 	public Rectangle playButton = new Rectangle(Game.WIDTH/4+120, 350, 100, 50);
-	//public Rectangle helpButton = new Rectangle (Game.WIDTH/4+ 120,450, 100, 50);
-	//public Rectangle quitButton = new Rectangle (Game.WIDTH/4+ 120, 550, 100, 50);
 
 	public void render(Graphics g){
 		
@@ -28,12 +26,6 @@ public class Menu{
 		
 		g.drawString("Play", playButton.x+19, playButton.y+35);
 		g2d.draw(playButton);
-		
-		//g.drawString("Help", helpButton.x+19, helpButton.y+35);
-		//g2d.draw(helpButton);
-		
-		//g.drawString("Quit", quitButton.x+19, quitButton.y+35);
-		//g2d.draw(quitButton);
 		
 	}	
 }
